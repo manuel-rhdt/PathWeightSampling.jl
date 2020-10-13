8ff01ab58adc09b4cb515c502c68da4a23d1747f
Manuel Reinhardt <M.Reinhardt@amolf.nl>
2020-10-13 16:41:38 +0200

    improve correlation plot

--- scripts/mcmc_correlations.jl
+++ scripts/mcmc_correlations.jl
@@ -18,13 +18,11 @@ signal = Trajectories.new_signal(initial, system)
 
 system.θ = 1.0
 
-samples, acceptance = Trajectories.generate_mcmc_samples(signal, system, 2^10, 2^16)
+
 
 using Plots
 using Statistics
 using StatsBase
-
-energies = Trajectories.energy.(samples, Ref(system))
 
 plot(energies)
 plot(autocor(energies))
@@ -33,13 +31,13 @@ mean(acceptance)
 
 block(arr) = 0.5 .* (arr[begin:2:end-1] .+ arr[begin+1:2:end])
 
-function plot_block_averages(values)
+function plot_block_averages!(p, values)
     a = values
 
     y_vals = Float64[]
     y_err = (Float64[], Float64[])
 
-    while length(a) > 10
+    while length(a) > 1
         normed_variance = var(a, corrected=false) / (length(a) - 1)
         variance_err = sqrt(2*normed_variance^2 / (length(a) - 1))
 
@@ -50,7 +48,14 @@ function plot_block_averages(values)
         a = block(a)
     end
 
-    plot((1:length(y_vals)), y_vals, yerror=y_err)
+    plot!(p, (1:length(y_vals)), y_vals, yerror=y_err)
 end
 
-plot_block_averages(energies)
+p = plot()
+for num_samples ∈ [14, 16, 18]
+    samples, acceptance = Trajectories.generate_mcmc_samples(signal, system, 2^10, 2^num_samples)
+    energies = Trajectories.energy.(samples, Ref(system))
+    plot_block_averages!(p, energies)
+end
+
+p
