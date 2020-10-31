5b89dd17c8d944eaade330f345b327846245d6b7
Manuel Reinhardt <manuel.rhdt@gmail.com>
2020-10-31 16:11:56 +0100

    improve code

--- scripts/basic_setup.jl
+++ scripts/basic_setup.jl
@@ -1,20 +1,11 @@
 using GaussianMcmc.Trajectories
 using Catalyst
 using DrWatson
-
-# sn = @reaction_network begin
-#     0.005, S --> ∅
-#     0.25, ∅ --> S
-# end
-
-# rn = @reaction_network begin
-#     0.01, S --> X + S
-#     0.01, X --> ∅ 
-# end
+using LinearAlgebra
 
 sn = @reaction_network begin
     κ, ∅ --> S
-    λ, s --> ∅
+    λ, S --> ∅
 end κ λ
 
 rn = @reaction_network begin
@@ -22,7 +13,23 @@ rn = @reaction_network begin
     μ, X --> ∅ 
 end ρ μ
 
-gen = Trajectories.configuration_generator(sn, rn, [0.005, 0.25], [0.01, 0.01])
+κ = 0.25
+λ = 0.005
+ρ = 0.01
+μ = 0.01
+mean_s = κ / λ
+mean_x = mean_s * ρ / μ
+
+# see Tostevin, ten Wolde, eq. 27
+sigma_squared_ss = mean_s
+sigma_squared_sx = ρ * mean_s / (λ + μ)
+sigma_squared_xx = mean_x * (1 + ρ / (λ + μ))
+
+joint_stationary = MvNormal([mean_s, mean_x], [sigma_squared_ss sigma_squared_sx; sigma_squared_sx sigma_squared_xx])
+signal_stationary = MvNormal([mean_s], sigma_squared_ss .* Matrix{Float64}(I, 1, 1))
+
+gen = Trajectories.configuration_generator(sn, rn, [κ, λ], [ρ, μ], signal_stationary, joint_stationary)
+
 (system, initial) = Trajectories.generate_configuration(gen; duration=500.0)
 
 nothing
--- scripts/posterior.jl
+++ scripts/posterior.jl
@@ -6,7 +6,7 @@ using LaTeXStrings
 
 grid_size = 100
 num_samples = 2^18
-θs = range(0,1,length=6)
+θs = range(0,1,length=3)
 
 resampled_samples = zeros((grid_size, num_samples, length(θs)))
 for (i, θ) in enumerate(θs)
@@ -25,30 +25,38 @@ end
 x = range(0.0, 500.0, length=grid_size)
 y = reshape(mean(resampled_samples, dims=2), (length(x), :))
 yerror = reshape(std(resampled_samples, dims=2), (length(x), :))
-yerror[:,1:5] .= 0.0
 
 pyplot()
 using Plots.PlotMeasures
-p1 = plot(x, y, 
-    ribbon=yerror, 
-    fillalpha=0.5, 
+p1 = plot(x, y[:,end],
+    ribbon=yerror[:,end], 
+    fillalpha=0.2, 
+    fill_z=1.0, 
+    ylim=(35,65),
+    linewidth=0,
+    label=nothing,
+    c=:viridis,
+)
+plot!(p1, initial, linewidth=0.9, label="orig. signal")
+plot!(p1, x, y,
+    fillalpha=0.4, 
     line_z=θs', 
     fill_z=θs', 
-    label=hcat(("$θ" for θ in θs)...), 
+    label=hcat(("θ=$θ" for θ in θs)...), 
     ylim=(35,65),
     linewidth=2.5,
-    c=:linear_blue_5_95_c73_n256,
     ylabel=L"S_t",
     xlabel=L"t",
     title="signals",
-    legend=false,
-    legendtitle=L"\theta",
+    legend=true,
     colorbar=false,
+    c=:viridis,
 )
 
-p2 = plot(system.response, c=:green, title="response", legend=:false)
 
-p = plot(p2, p1, layout=Plots.grid(2,1), size=(250,600))
+p2 = plot(system.response, c=:green, title="response", legend=:false, ylabel=L"X_t",ylim=(35,65))
+
+p = plot(p2, p1, layout=Plots.grid(2,1), size=(600,600))
 pos = p.o.axes[3].get_position()
 pos.y1 *= 0.5
 p.o.axes[3].set_position(pos)
