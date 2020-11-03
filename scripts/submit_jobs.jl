ae77ae992de839dc253e4e61e01165de31a7546f
Manuel Reinhardt <M.Reinhardt@amolf.nl>
2020-11-03 13:47:57 +0100

    prepare run

--- scripts/submit_jobs.jl
+++ scripts/submit_jobs.jl
@@ -1,13 +1,14 @@
 using DrWatson
 import JSON
 using Random
+using FileIO
 
 my_args = Dict(
     "algorithm" => "thermodynamic_integration",
     "run_name" => "2020-11-03_stationary_sweep",
-    "duration" => 2 .^ range(log2(20), log2(500), length=6),
+    "duration" => 2 .^ range(log2(20), log2(200), length=6),
     "num_responses" => 1000,
-    "mean_s" => [20, 50, 100, 200],
+    "mean_s" => [20, 40, 70, 100],
     "corr_time_s" => 100,
     "corr_time_ratio" => 5,
 )
@@ -35,35 +36,46 @@ end
 
 dicts = dict_list(my_args)
 
+function DrWatson._wsave(filename, d::Dict)
+    if endswith(filename, ".json")
+        open(filename, "w") do io
+            JSON.print(io, d)
+        end
+    else
+        FileIO.save(filename, d)
+    end
+end
+
+filenames = tmpsave(dicts, suffix="json")
+
 out_dir = projectdir("data", "output")
 mkpath(out_dir)
 
-function submit_job_array(dict, njobs)
-    jobarray = collect(1:njobs)
-
-    dict = copy(dict)
-
-    dict["N"] = jobarray
-    array_dicts = dict_list(dict)
-
-    filename = runsave(dicts)
+function submit_job_array(filename, njobs, runtime)
     jobscript = """
         export JULIA_PROJECT=$(projectdir())
 
         julia -e "using InteractiveUtils; versioninfo(verbose=true)"
-        julia -O3 $(projectdir("scripts", "simple_network.jl")) $filename.\$PBS_ARRAYID.json
+        julia -O3 $(projectdir("scripts", "simple_network.jl")) $filename.json
     """
     
     result = ""
-    open(`qsub -N Sweep_S -l nodes=1:ppn=1:highcore,mem=4gb,walltime=24:00:00 -t 1-$njobs -j oe -o $out_dir`, "r+") do io
+    open(`qsub -N Sweep_S -l nodes=1:ppn=1:highcore,mem=4gb,walltime=$runtime -t 1-$njobs -j oe -o $out_dir`, "r+") do io
         print(io, jobscript)
         close(io.in)
         result *= read(io, String)
     end
     
-    print(result)
+    println(result)
+end
+
+function estimate_runtime(dict)
+    factor = 0.0015 * 1.8
+    constant = 5 * 60
+    round(Int, factor * dict["mean_s"] * dict["duration"] * dict["num_responses"] + constant)
 end
 
 for d in dicts
-    submit_job_array(d, 100)
+    runtime = estimate_runtime(d)
+    submit_job_array(d, 100, runtime)
 end
