630fd0de5f792bdc6a48cc01b7b6dd8b5f108c67
Manuel Reinhardt <manuel.rhdt@gmail.com>
2020-11-08 19:05:52 +0100

    Improve floating point handling

--- src/trajectories/distribution.jl
+++ src/trajectories/distribution.jl
@@ -15,6 +15,9 @@ distribution(rn::ReactionSystem, log_p0) = TrajectoryDistribution(create_chemica
 @fastmath function logpdf(dist::TrajectoryDistribution{<:Tuple}, trajectory; params=[])::Float64
     ((uprev, tprev), state) = iterate(trajectory)
     result = dist.log_p0(uprev...)::Float64
+    if !isfinite(result)
+        return -Inf
+    end
 
     for (u, t) in Iterators.rest(trajectory, state)
         dt = t - tprev
