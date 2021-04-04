4ecc75faadc62dffc7dd78e45e0912c6b6cadff6
Manuel Reinhardt <manuel.rhdt@gmail.com>
2021-04-04 12:00:42 +0200

    Fix sign error

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,6 @@ using Test
 @testset "Events" begin include("events.jl") end
 @testset "TrajectoryTests" begin include("trajectories.jl") end
 @testset "TrajectoryProbability" begin include("probability.jl") end
-# @testset "Annealing" begin include("annealing.jl") end
-# @testset "Gaussian" begin include("gaussian.jl") end
+@testset "Gaussian" begin include("gaussian.jl") end
 @testset "SRXsystem" begin include("SRXsystem.jl") end
 @testset "SMCEstimate" begin include("SMCEstimate.jl") end
