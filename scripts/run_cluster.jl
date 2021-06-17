069ffaeb694319d2fd2ea8eaa5a5e121caa41b76
Manuel Reinhardt <manuel.rhdt@gmail.com>
2021-06-17 14:32:43 +0200

    fix bug

--- scripts/run_cluster.jl
+++ scripts/run_cluster.jl
@@ -6,7 +6,7 @@ nprocs = length(readlines(ENV["PBS_NODEFILE"]))
 manager = MPIManager(np=nprocs, master_tcp_interface=gethostname())
 
 # addprocs(manager)
-addprocs(manager, exename=`$(Sys.BINDIR)/julia -J$(pwd())/GMcmcSysimage.so`)
+addprocs(manager, exename=`$(Sys.BINDIR)/julia -J$(pwd())/GaussianMcmc.jl/GMcmcSysimage.so`)
 
 using Logging
 @info "Successfully launched MPI processes"
