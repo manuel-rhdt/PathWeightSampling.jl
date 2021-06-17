a22c2c2dd3aaa792ed9486f77f3d73a0fb6ef72c
Manuel Reinhardt <manuel.rhdt@gmail.com>
2021-06-17 14:27:46 +0200

    fix for DrWatson update

--- scripts/simple_network.jl
+++ scripts/simple_network.jl
@@ -7,7 +7,7 @@
 end
 
 f = ARGS[1]
-dict = JSON.parsefile(projectdir("_research", "tmp", f))
+dict = JSON.parsefile(projectdir("_research", "tmp", f))["params"]
 @info "Read file" file = projectdir("_research", "tmp", f)
 
 duration = dict["duration"]
