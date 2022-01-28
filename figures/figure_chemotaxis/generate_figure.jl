bcc1b741cd0331d44020ca993b6d444df4d9ee3b
Manuel Reinhardt <manuel.rhdt@gmail.com>
2022-01-28 14:28:59 +0100

    update figure

--- figures/figure_chemotaxis/generate_figure.jl
+++ figures/figure_chemotaxis/generate_figure.jl
@@ -2,19 +2,20 @@ using DrWatson
 using DataFrames
 using Statistics
 using CairoMakie
-using HDF5
 using LaTeXStrings
 using CSVFiles
 using Dates
 using Interpolations
 
 sims = DataFrame(load(projectdir("figures", "figure_chemotaxis", "data", "simulation_2021-10-12.csv")))
+sims_wo_methylation = DataFrame(load(projectdir("figures", "figure_chemotaxis", "data", "simulation_2022-01-27.csv")))
 imi_data = DataFrame(load(projectdir("figures", "figure_chemotaxis", "data", "imi.csv")))
 imi_data_no_methylation = DataFrame(load(projectdir("figures", "figure_chemotaxis", "data", "imi_no_methylation.csv")))
 gaus_data = DataFrame(load(projectdir("figures", "figure_chemotaxis", "data", "gaussian_rate.csv")))
 
 normalfont = projectdir("figures", "fonts", "NotoSans-Condensed.ttf")
 boldfont = projectdir("figures", "fonts", "NotoSans-CondensedSemiBold.ttf")
+subfigurelabelfont = projectdir("figures", "fonts", "NotoSans-Bold.ttf")
 
 plot_theme = Theme(
     font = normalfont,
@@ -24,7 +25,7 @@ plot_theme = Theme(
     linewidth = 2,
     markersize = 5,
     spinewidth = 0.5,
-    figure_padding = (5, 5, 10, 5),
+    figure_padding = (2, 2, 2, 2),
     Axis = (
         spinewidth = 1.0,
         xgridwidth = 1.0,
@@ -43,9 +44,12 @@ plot_theme = Theme(
     Legend = (
         framevisible = false,
         framewidth = 1.0,
-        rowgap = 0,
+        rowgap = 2,
         titlegap = 2,
         titlefont = boldfont,
+        titlehalign = :left,
+        halign = :center,
+        gridshalign = :left,
         titlefontsize = 11,
         groupgap = 5,
         patchsize = (10, 5),
@@ -55,7 +59,7 @@ plot_theme = Theme(
 )
 set_theme!(plot_theme)
 
-fig = Figure(resolution = (246, 270))
+fig = Figure(resolution = (246, 250))
 ax1 = Axis(fig[1, 1], xlabel = "traj. duration (s)", title = "mutual\ninformation (bits)")
 
 groups = filter(g -> g.TauL[1] ∈ [0.01, 0.1, 1.0, 10.0], groupby(sims, [:M, :TauL], sort = true))
@@ -65,7 +69,7 @@ colors = cmap[3:2:9]
 for (g, c) in zip(groups, colors)
     lines!(ax1, g.DiscreteTimes, g.Mean ./ log(2), label = "$(g.TauL[1]) s", color = c)
 end
-Legend(fig[1, 3], ax1, "input\ncorr.\ntime", orientation = :vertical, tellwidth = true)
+Legend(fig[1, 3], ax1, "input\ncorrelation\ntime", orientation = :vertical, tellwidth = true)
 # colgap!(fig.layout, 2, Fixed(-35))
 
 ax2 = Axis(fig[1, 2], xlabel = "traj. duration (s)", title = "information\n rate (bits/s)")
@@ -93,20 +97,29 @@ ax3 = Axis(
 
 lines!(ax3, gaus_data.TauL, gaus_data.InfoRate, color = :green, label = "Gaussian")
 
-xvals = Float64[]
-yvals = Float64[]
-for g in groupby(sims, [:M, :TauL], sort = true)
-    push!(xvals, g.TauL[1])
-    indexfrom = searchsortedfirst(g.DiscreteTimes, 150)
-    rates = diff(g.Mean) ./ diff(g.DiscreteTimes) ./ log(2)
-    asymptotic_rate = mean(rates[indexfrom:end])
-    push!(yvals, asymptotic_rate)
+function get_info_rates(sims)
+    xvals = Float64[]
+    yvals = Float64[]
+    for g in groupby(sims, [:M, :TauL], sort = true)
+        push!(xvals, g.TauL[1])
+        indexfrom = searchsortedfirst(g.DiscreteTimes, 150)
+        rates = diff(g.Mean) ./ diff(g.DiscreteTimes) ./ log(2)
+        asymptotic_rate = mean(rates[indexfrom:end])
+        push!(yvals, asymptotic_rate)
+    end
+    xvals, yvals
 end
+
+xvals, yvals = get_info_rates(sims)
+lines!(ax3, xvals, yvals, color = :black, label = "PWS")
+
+xvals, yvals = get_info_rates(sims_wo_methylation)
+lines!(ax3, xvals, yvals, color = :black, linestyle = :dot, label = "push-pull")
+
 ax3.xticks = [0.01, 0.1, 1.0, 10.0]
 ax3.xtickformat = "{:.2f}"
-lines!(ax3, xvals, yvals, color = :black, label = "PWS")
-ylims!(ax3, 0, 19)
-text!(ax3, "information transmission rate", position = Point(1e-2, 15), textsize = 8, align = (:left, :baseline), font = "/Users/mr/Library/Fonts/NotoSans-CondensedSemiBold.ttf")
+ylims!(ax3, 0, 25)
+text!(ax3, "information transmission rate", position = Point(1e-2, 20), textsize = 8, align = (:left, :baseline), font = boldfont)
 
 c4 = :black
 ax4 = Axis(
@@ -127,21 +140,21 @@ hidespines!(ax4, :t)
 # hidexdecorations!(ax4)
 # hideydecorations!(ax4, label = false, ticks = false, ticklabels = false)
 
-lines!(ax4, imi_data.TauL, imi_data.IMI, color = c4, label = "exact")
-lines!(ax4, gaus_data.TauL, gaus_data.InstantaneousInfo, color = :green, label = "LNA")
+lines!(ax4, imi_data.TauL, imi_data.IMI, color = c4, label = "chemotaxis")
 lines!(ax4, imi_data_no_methylation.TauL, imi_data_no_methylation.IMI, color = c4, linestyle = :dot, label = "push-pull")
+lines!(ax4, gaus_data.TauL, gaus_data.InstantaneousInfo, color = :green, label = "Gaussian\n(LNA)")
 ylims!(ax4, 0, 1.9)
-# annotations!(ax4, ["no methylation"], [Point2(4, 1.7)], font = "/Users/mr/Library/Fonts/NotoSans-CondensedBold.ttf", align = (:right, :center), textsize = 7, rotation = 0.3)
+# annotations!(ax4, ["no methylation"], [Point2(4, 1.7)], font = boldfont, align = (:right, :center), textsize = 7, rotation = 0.3)
 
 Legend(fig[2, 3], ax4, "systems", orientation = :vertical, tellwidth = true)
 
-rowsize!(fig.layout, 2, Auto(1.3))
+rowsize!(fig.layout, 2, Auto(1.7))
 
 for (label, layout) in zip(["b", "c", "d"], [fig[1, 1], fig[1, 2], fig[2, 1:3]])
     Label(layout[1, 1, TopLeft()], label,
         textsize = 14,
-        font = "Noto Sans Bold",
-        padding = (3, 5, 2, -5),
+        font = subfigurelabelfont,
+        padding = (3, 5, 4, -5),
         halign = :left)
 end
 
--- figures/figure_gene_expr/figure_gene_expr.jl
+++ figures/figure_gene_expr/figure_gene_expr.jl
@@ -6,10 +6,10 @@ using DrWatson
 ddir = projectdir("figures", "figure_gene_expr", "data")
 
 comb_data = DataFrame(load(joinpath(ddir, "gene-expression_2021-10-04.csv")))
-dmc_data = comb_data[comb_data.Alg .== "Direct MC", :]
-smc_data = comb_data[comb_data.Alg .== "SMC", :]
-smc_estimate = groupby(smc_data, :M)[(;M=128)]
-ti_data = comb_data[(comb_data.Alg .== "TI") .& (comb_data.Duration .== comb_data.DiscreteTimes), :]
+dmc_data = comb_data[comb_data.Alg.=="Direct MC", :]
+smc_data = comb_data[comb_data.Alg.=="SMC", :]
+smc_estimate = groupby(smc_data, :M)[(; M = 128)]
+ti_data = comb_data[(comb_data.Alg.=="TI").&(comb_data.Duration.==comb_data.DiscreteTimes), :]
 zechner_data = DataFrame(load(joinpath(ddir, "gene-expression_2021-06-18_zechner.csv")))
 gaussian_data = DataFrame(load(joinpath(ddir, "gene-expression_2021-07-19_gaussian.csv")))
 
@@ -59,112 +59,112 @@ normalfont = projectdir("figures", "fonts", "NotoSans-Condensed.ttf")
 titlefont = projectdir("figures", "fonts", "NotoSans-SemiBoldItalic.ttf")
 
 fontsize_theme = Theme(
-    font = normalfont, 
-    fontsize = 8, 
-    colgap=3, 
-    linewidth=2, 
-    markersize=6,
-    spinewidth=0.5,
+    font = normalfont,
+    fontsize = 8,
+    colgap = 3,
+    linewidth = 2,
+    markersize = 6,
+    spinewidth = 0.5,
     Axis = (
-        spinewidth=1.0,
-        xgridwidth=1.0,
-        ygridwidth=1.0,
-        xtickwidth=1.0,
-        ytickwidth=1.0,
-        xticksize=3, 
-        yticksize=3,
-        xlabelpadding=0,
-        ylabelpadding=5,
-        xticklabelpad=0,
-        yticklabelpad=3,
+        spinewidth = 1.0,
+        xgridwidth = 1.0,
+        ygridwidth = 1.0,
+        xtickwidth = 1.0,
+        ytickwidth = 1.0,
+        xticksize = 3,
+        yticksize = 3,
+        xlabelpadding = 0,
+        ylabelpadding = 5,
+        xticklabelpad = 0,
+        yticklabelpad = 3,
     ),
     Legend = (
-        framewidth=1.0,
-        rowgap=0, 
-        titlegap=2, 
-        groupgap=5,
-        patchsize=(11,10),
-        padding=(3,3,3,3),
-        linewidth=1.25,
-        gridshalign=:left,
-        titlehalign=:left,
-        titlefont=titlefont,
-        framevisible=false
+        framewidth = 1.0,
+        rowgap = 0,
+        titlegap = 2,
+        groupgap = 5,
+        patchsize = (11, 10),
+        padding = (3, 3, 3, 3),
+        linewidth = 1.25,
+        gridshalign = :left,
+        titlehalign = :left,
+        titlefont = titlefont,
+        framevisible = false
     )
 )
 
-fig = with_theme(fontsize_theme) do 
-    f = Figure(resolution=(246, 1.4*146), figure_padding = 2)
-    Axis(f[1, 1], xlabel="trajectory duration", ylabel="mutual information (nats)")
+fig = with_theme(fontsize_theme) do
+    f = Figure(resolution = (246, 1.4 * 146), figure_padding = 2)
+    Axis(f[1, 1], xlabel = "trajectory duration", ylabel = "mutual information (nats)")
 
-    groups = collect(groupby(dmc_data, :M; sort=true))
+    groups = collect(groupby(dmc_data, :M; sort = true))
 
     m_label = ["M = $(g.M[1])" for g in groups]
-    cmap = cgrad(:linear_kgy_5_95_c69_n256, rev=true)
-    colors = [cmap[i / length(groups) * 0.7] for i = 1:length(groups)]
-    ti_colors = [cgrad(:lajolla)[i] for i = range(0.2, 0.6, length=3)]
+    cmap = cgrad(:linear_kgy_5_95_c69_n256, rev = true)
+    colors = [cmap[i/length(groups)*0.7] for i = 1:length(groups)]
+    ti_colors = [cgrad(:lajolla)[i] for i = range(0.2, 0.6, length = 3)]
     pathmi_color = :orangered
 
     for (i, group) in enumerate(groups)
         label = "$(group.M[1])"
-        lines!(group.DiscreteTimes, group.Mean, linewidth=0.75, label=label, color=colors[i])
+        lines!(group.DiscreteTimes, group.Mean, linewidth = 0.75, label = label, color = colors[i])
     end
 
-    group_color = [LineElement(color=color) for color in colors]
-    smc_color = [LineElement(color=:black)]
+    group_color = [LineElement(color = color) for color in colors]
+    smc_color = [LineElement(color = :black)]
 
-    lines!(smc_estimate.DiscreteTimes, smc_estimate.Mean, label="SMC", linewidth=1.7, color=:black)
-    lines!(zechner_data.Duration, zechner_data.PMI, linewidth=1.5, linestyle=:dash, color=pathmi_color)
-    lines!(gaussian_data.DiscreteTimes, gaussian_data.Value, linewidth=1.5, linestyle=:dot, color=:MidnightBlue)
+    lines!(smc_estimate.DiscreteTimes, smc_estimate.Mean, label = "SMC", linewidth = 1.7, color = :black)
+    lines!(zechner_data.Duration, zechner_data.PMI, linewidth = 1.5, linestyle = :dash, color = pathmi_color)
+    lines!(gaussian_data.DiscreteTimes, gaussian_data.Value, linewidth = 1.5, linestyle = :dot, color = :MidnightBlue)
 
     ti_label = []
     ti_color = []
-    for (i, group) in enumerate(groupby(ti_data, :M, sort=true))
-        scatter!(group.DiscreteTimes, group.Mean, marker=:cross, color=ti_colors[i])
+    for (i, group) in enumerate(groupby(ti_data, :M, sort = true))
+        scatter!(group.DiscreteTimes, group.Mean, marker = :cross, color = ti_colors[i])
         push!(ti_label, "M = $(group.M[1])")
-        push!(ti_color, MarkerElement(marker=:cross, color=ti_colors[i]))
+        push!(ti_color, MarkerElement(marker = :cross, color = ti_colors[i]))
     end
 
-    approx_entry = [LineElement(linestyle=:dash, color=pathmi_color), LineElement(linestyle=:dot, color=:MidnightBlue)]
+    approx_entry = [LineElement(linestyle = :dash, color = pathmi_color), LineElement(linestyle = :dot, color = :MidnightBlue)]
 
     legend = Legend(
-        f[1, 2], 
-        [group_color, ti_color, smc_color, approx_entry], 
-        [m_label, ti_label, 
-        ["M = 128"], ["Duso, et. al. (2019)", "linear noise"]], 
-        ["DPWS", "TI-PWS", "RR-PWS", "approximations"], 
+        f[1, 2],
+        [group_color, ti_color, smc_color, approx_entry],
+        [m_label, ti_label,
+            ["M = 128"], ["Duso, et. al. (2019)", "Gaussian (LNA)"]],
+        ["DPWS", "TI-PWS", "RR-PWS", "approximations"],
     )
     xlims!(0.0, 10.2)
     ylims!(0.0, 42)
 
     inset_lims = (3.3, 8.5)
-    lines!([(0,0), (inset_lims[1], 0), inset_lims, (0, inset_lims[2]), (0,0)], color=:black, linewidth=0.75, linestyle=:dot)
+    lines!([(0, 0), (inset_lims[1], 0), inset_lims, (0, inset_lims[2]), (0, 0)], color = :black, linewidth = 0.75, linestyle = :dot)
 
     inset_ax = Axis(f[1, 1],
-        width=Relative(0.55),
-        height=Relative(0.33),
-        halign=0.25,
-        valign=0.95,
-        xtickalign=1.0,
-        ytickalign=1.0,
-        backgroundcolor=:white,
-        xticksize=2,
-        xticklabelsize=7,
-        xgridvisible=true,
-        yticksize=2,
-        yticklabelsize=7,
+        width = Relative(0.55),
+        height = Relative(0.33),
+        halign = 0.25,
+        valign = 0.95,
+        xtickalign = 1.0,
+        ytickalign = 1.0,
+        backgroundcolor = :white,
+        xticksize = 2,
+        xticklabelsize = 7,
+        xgridvisible = true,
+        yticksize = 2,
+        yticklabelsize = 7,
     )
 
     for (i, group) in enumerate(groups)
         label = "$(group.M[1])"
-        lines!(inset_ax, group.DiscreteTimes, group.Mean, linewidth=0.5, label=label, color=colors[i])
+        lines!(inset_ax, group.DiscreteTimes, group.Mean, linewidth = 0.5, label = label, color = colors[i])
     end
-    lines!(inset_ax, smc_estimate.DiscreteTimes, smc_estimate.Mean, linewidth=1.0, color=:black)
-    lines!(inset_ax, zechner_data.Duration, zechner_data.PMI, linewidth=0.75, linestyle=:dash, color=pathmi_color)
-    lines!(inset_ax, gaussian_data.DiscreteTimes, gaussian_data.Value, linewidth=1.5, linestyle=:dot, color=:MidnightBlue)
+    lines!(inset_ax, smc_estimate.DiscreteTimes, smc_estimate.Mean, linewidth = 1.0, color = :black)
+    lines!(inset_ax, zechner_data.Duration, zechner_data.PMI, linewidth = 0.75, linestyle = :dash, color = pathmi_color)
+    lines!(inset_ax, gaussian_data.DiscreteTimes, gaussian_data.Value, linewidth = 1.5, linestyle = :dot, color = :MidnightBlue)
 
-    for (i, group) in enumerate(groupby(ti_data, :M, sort=true))
-        scatter!(inset_ax, group.DiscreteTimes, group.Mean, marker=:cross, color=ti_colors[i])
+    for (i, group) in enumerate(groupby(ti_data, :M, sort = true))
+        scatter!(inset_ax, group.DiscreteTimes, group.Mean, marker = :cross, color = ti_colors[i])
     end
 
     xlims!(inset_ax, 0, inset_lims[1])
@@ -178,3 +178,6 @@ fig = with_theme(fontsize_theme) do
 end
 
 save(projectdir("figures", "figure_gene_expr", "gene-expr-figure.svg"), fig, pt_per_unit = 1)
+save(projectdir("figures", "figure_gene_expr", "gene-expr-figure.pdf"), fig, pt_per_unit = 1)
+
+fig
